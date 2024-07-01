bae89d6ae77f19927e33e8e216bce514e5cc3883
NamTNDEV <namtndev312002@gmail.com>
2024-07-01 08:16:04 +0700

    #001 - Cài đặt ESLint, Prettier, TailwindCSS cho dự án.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident rerum impedit ea iusto voluptas quibusdam
+        maxime reiciendis quos, pariatur, ullam dolorum, adipisci saepe fugiat nostrum eaque. Aliquid alias veritatis
+        earum. Labore tempore quaerat eum iusto doloribus voluptate quasi ad consequuntur corporis aut nam soluta atque
+        culpa, possimus, laudantium exercitationem veniam nobis! Dolore excepturi officia distinctio pariatur veniam
+        ullam ad aspernatur. Modi ab quasi reiciendis et! Quae itaque et, eius non minima vero tenetur ratione! Pariatur
+        atque nisi amet incidunt asperiores consequatur quo deserunt libero quidem porro expedita beatae, animi hic!
+        Omnis dolor laboriosam quia sed? Aspernatur hic illum natus suscipit, eaque tempora voluptatem sit ea maxime ex
+        doloremque illo, odio eius! Consequuntur voluptate at, optio magnam recusandae eius. Ratione, corrupti!
+        Obcaecati ut quia aliquid labore voluptatum, recusandae soluta provident? Ab eveniet eaque, fugit quod cum sequi
+        corporis deleniti. Dolore neque eum sunt molestiae totam magnam porro minima est doloremque recusandae! Quas sit
+        laboriosam illum praesentium pariatur, doloremque dolorem iure totam rerum, maiores quidem. Soluta fuga, sint
+        dolorum libero iure eligendi obcaecati totam voluptatibus numquam cupiditate. Dolorem quaerat assumenda beatae
+        numquam? Non vel, itaque necessitatibus ducimus adipisci consectetur accusantium exercitationem quis dolor
+        asperiores corrupti facere facilis eos porro cum. Unde, quae illum commodi explicabo voluptatum porro?
+        Distinctio repudiandae minima quidem accusamus. Est nulla, neque eveniet nesciunt aliquid similique corrupti
+        fuga odio possimus, rem ipsa numquam dicta cupiditate. Ipsa iste illo ut numquam obcaecati dolorum delectus sed
+        molestias, eligendi omnis laborum libero? Quos deserunt debitis maiores. Eius asperiores aspernatur dicta, est
+        eaque earum cum quam quas similique. Veritatis, tempore unde sapiente iste necessitatibus sed quo, voluptas
+        eaque quia quos eos ratione dolores? Doloribus qui magnam totam. Alias vero iure architecto sunt assumenda
+        laborum tenetur nulla ratione voluptatibus maxime voluptates aliquid illo ducimus, explicabo voluptas itaque
+        blanditiis dicta tempore eveniet asperiores pariatur perferendis!
+      </h1>
+    </main>
   )
 }
 
