47d5ae84f08e5979d2c0853a42e5175c8f19d04a
NamTNDEV <namtndev312002@gmail.com>
2024-07-02 14:46:22 +0700

    #004 - Validate cho form Login và Register.

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/css/reset.css'
 import './assets/css/index.css'
-import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,39 +1,62 @@
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { getRules } from 'src/utils/rules'
 
-function LoginPage() {
+interface FormData {
+  email: string
+  password: string
+  confirm_password: string
+}
+
+function RegisterPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const rules = getRules()
+
+  const onsubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
     <div className='bg-primary'>
-      <div className='max-w-7xl mx-auto px-4'>
+      <div className='container'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
-              <div className='text-2xl'>Đăng nhập</div>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onsubmit} noValidate>
+              <div className='text-2xl'>Đăng Nhập</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Email'
+                  {...register('email', rules.email)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.email?.message}</div>
               </div>
               <div className='mt-3'>
                 <input
                   type='password'
-                  name='password'
                   className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
                   placeholder='Password'
+                  {...register('password', rules.password)}
                 />
-                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
+                <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm'>{errors.password?.message}</div>
               </div>
               <div className='mt-3'>
-                <button className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'>
+                <button
+                  type='submit'
+                  className='w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'
+                >
                   Đăng nhập
                 </button>
               </div>
               <div className='flex items-center justify-center mt-8'>
-                <span className='text-gray-400'>Bạn chưa có tài khoản?</span>
-                <Link className='text-red-400 ml-1' to='/register'>
+                <span className='text-gray-400'>Bạn đã có tài khoản?</span>
+                <Link className='text-red-400 ml-1' to='/login'>
                   Đăng ký
                 </Link>
               </div>
@@ -45,4 +68,4 @@ function LoginPage() {
   )
 }
 
-export default LoginPage
+export default RegisterPage
